12f681b032a91c999927558a9c93a0f5cf210e4d
Erik <carloserik18@hotmail.com>
2024-11-23 11:16:04 -0300

    Project created

---
+++ src/main/java/com/deverik/IRRF/IrrfApplication.java
@@ -0,0 +1,13 @@
+package com.deverik.IRRF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrrfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deverik/IRRF/IrrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deverik.IRRF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
